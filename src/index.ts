6ec49af2cbf719a8e34d7c62ba9212b2b1d04efd
Matt Bierner <matb@microsoft.com>
2019-01-25 17:20:54 -0800

    Init

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Foo } from './interface';
+
+{
+    const foo: Foo = { x: undefined }
+    const prop = foo.x
+    console.log(prop)
+}
---
+++ src/interface.ts
@@ -0,0 +1,6 @@
+import { Class } from './class';
+
+export interface Foo {
+    x: Class;
+}
+
